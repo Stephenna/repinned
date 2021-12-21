a4aa55bee85b41bb68b2d9377698d23512fa19e8
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-12-21 18:40:24 -0500

    deployment prep

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-
 // routes
-const pinRoute = require('./routes/pinRoute')
-const userRoute = require('./routes/userRoute');
-
-
+const pinRoute = require("./routes/pinRoute");
+const userRoute = require("./routes/userRoute");
 
 dotenv.config();
 
@@ -20,28 +17,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // connect to the database
 mongoose
-.connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-})
-.then(console.log(`👽 Connected to DB!`))
-.catch(e => console.log(`Error connecting to DB: ${e}`))
-
+  })
+  .then(console.log(`👽 Connected to DB!`))
+  .catch((e) => console.log(`Error connecting to DB: ${e}`));
 
 app.use("/pins", pinRoute);
 app.use("/users", userRoute);
 
+// app.use(express.static(path.join(__dirname, "client", "build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("API running")
+    })
+}
 
-// Listening 
-app.listen(process.env.PORT || 8000  , () => {
-    console.log(`🚀 Server is running!`)
-})
+// Listening
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`🚀 Server is running!`);
+});
